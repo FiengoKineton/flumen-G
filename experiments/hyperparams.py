39938c2d67d89bd86bd1088433ca2ac0debb23e3
Giuseppe Fiengo <g7.fiengo@gmail.com>
2025-04-17 01:01:34 +0200

    .

--- experiments/hyperparams.py
+++ experiments/hyperparams.py
@@ -255,7 +255,7 @@ class Hyperparams:
                 "sched_patience": 10,
                 "sched_factor": 3,                  # 2,
                 "loss": "mse",                  
-                "discretisation_mode": "TU", 
+                "discretisation_mode": "BE",        # TU
                 "optimiser_mode": "adam",       
                 "x_update_mode": "entropy",
                 "mode_rnn": "new", 
